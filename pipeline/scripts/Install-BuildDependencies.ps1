435d8a82598c7953ea10c435c40248dd74b0890a
Michael Romer <mvromer@gmail.com>
2018-06-21 16:08:22 -0500

    Attempting to force reload of PSGet after it has been updated

--- pipeline/scripts/Install-BuildDependencies.ps1
+++ pipeline/scripts/Install-BuildDependencies.ps1
@@ -3,7 +3,12 @@ param()
 
 $ErrorActionPreferenc = "Stop"
 
+# Update to the latest PowerShellGet so that we have prerelease support and make sure we have that module loaded.
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PowerShellGet -Force
+Remove-Module -Name PowerShellGet -Force
+Import-Module -Name PowerShellGet
+
+# Install the rest of our build dependencies.
 Install-Module -Name Psake
 Install-Module -Name Pester -Force -SkipPublisherCheck
