461363a880cf8fdb40ddc0cb8d1d62b086cb815e
Michael Romer <mvromer@gmail.com>
2018-06-21 16:03:04 -0500

    Added PSGet to list of build deps that are installed

--- pipeline/scripts/Install-BuildDependencies.ps1
+++ pipeline/scripts/Install-BuildDependencies.ps1
@@ -4,5 +4,6 @@ param()
 $ErrorActionPreferenc = "Stop"
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name PowerShellGet -Force
 Install-Module -Name Psake
-Install-Module -Name Pester
+Install-Module -Name Pester -Force -SkipPublisherCheck
