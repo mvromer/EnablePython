14105688ab7083684b1a3ebd1b65326368d5ef14
Michael Romer <mvromer@gmail.com>
2018-06-22 00:20:40 -0500

    Another attempt at getting the PSGet update to work properly

--- pipeline/scripts/Install-BuildDependencies.ps1
+++ pipeline/scripts/Install-BuildDependencies.ps1
@@ -1,6 +1,9 @@
 [CmdletBinding()]
 param()
 
-Get-Module
+# According to https://github.com/PowerShell/PowerShellGet/issues/246#issuecomment-376346105, to update PSGet in an
+# automated build, we should do the update in a separate PowerShell session.
+powershell.exe -Command { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
+powershell.exe -Command { Install-Module -Name PowerShellGet -Force }
 Install-Module -Name Psake
 Install-Module -Name Pester -Force -SkipPublisherCheck
